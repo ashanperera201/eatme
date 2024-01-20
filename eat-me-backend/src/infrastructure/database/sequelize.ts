3003b637d63560bcdf0ae5bc308251c56b8d8e90
ashanperera201 <AshanPerera.201@gmail.com>
2024-01-20 11:54:50 +0530

    adding env changes.

--- eat-me-backend/src/infrastructure/database/sequelize.ts
+++ eat-me-backend/src/infrastructure/database/sequelize.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize('eat_me', 'root', 'pass#word123', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER_NAME, process.env.DB_USER_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
 });
 
